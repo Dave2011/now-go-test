12d3eaf92ec228e8a1fda8885b0d7940f86dd960
David Forster <daveforster.de@googlemail.com>
2019-05-16 21:41:52 +0200

    Next

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
-package now_go_test
+package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "Hello World") }
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", Handler).Methods("GET")
+	log.Fatal(http.ListenAndServe(":3030", router))
+}
+func Handler(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "Wow, SUCH Test") }
